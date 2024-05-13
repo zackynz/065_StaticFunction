424a7ff6b2e899665e6dd2b66194fa740c0fc195
zackynz <nurasfanz@gmail.com>
2024-05-13 19:25:01 +0700

    angka belajarcpp(3): Membuat objek belajarcpp bertipe angka menggunakan konstruktor berparameter, mengalokasikan memori untuk array berukuran 3.

--- mbrDestruktor.cpp
+++ mbrDestruktor.cpp
@@ -8,3 +8,41 @@ private:
 public:
     angka(int); //constructor
     ~angka(); // Destruktor
+    void cetakData();
+    void isiData();
+};
+//Definisi member Function
+angka::angka(int i){ //construktor
+    panjang = i;
+    arr = new int[i];
+    isiData();
+
+}
+
+angka::~angka(){ //destruktor
+    cout<<endl;
+    cetakData();
+    delete[]arr;
+    cout<<"Alamat Array Sudah Dilepaskan"<<endl;
+}
+
+void angka::cetakData(){
+    for(int i=1;i<=panjang;i++){
+        cout<<i<<" = "<<arr[i]<<endl;
+    }
+}
+
+void angka::isiData(){
+    for(int i=1;i<=panjang;i++){
+        cout<<i<<" = ";cin>>arr[i];
+    }
+    cout<<endl;
+}
+
+int main(){
+    angka belajarcpp(3); //constructor dipanggil
+    angka *ptrBelajarcpp = new angka(5); //constructor dipanggil
+    delete ptrBelajarcpp; //destructor dipanggil
+
+    return 0;
+}//destructor dipanggil
