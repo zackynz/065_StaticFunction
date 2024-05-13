b1b6acf16c114037715e2671a31fcfd36f757ad8
zackynz <nurasfanz@gmail.com>
2024-05-13 15:08:07 +0700

    menggunakan int dengan jumlah 100

--- mbtStaticfunction.cpp
+++ mbtStaticfunction.cpp
@@ -13,5 +13,14 @@ public:
 
     void setID();
     void printA11();
+
+    static void setNim(int pNim) { nim = pNim; }
+    static int getNim() { return nim; }
+
+    mahasiswa (string pnama):nama(pnama)
+    {
+        setID();
+    }
 };
 
+int mahasiswa::nim = 100;
