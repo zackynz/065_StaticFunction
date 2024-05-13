888112128ec18f7f06fa7d35a9dc588af393aa72
zackynz <nurasfanz@gmail.com>
2024-05-13 15:43:05 +0700

    mengembalikan nilai pda cetak mahasiswa

--- mbrconstruktor.cpp
+++ mbrconstruktor.cpp
@@ -17,7 +17,33 @@ private:
     Mahasiswa(int iNim)
     {
         nim = iNim;
-    }
-
-
+    
+    };
+    Mahasiswa(string);
+    Mahasiswa(int iNim, string iNama);
+    void cetak();
 };
+
+Mahasiswa::Mahasiswa(string iNama){
+    nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama)
+    {
+        nim = iNim;
+        nama = iNama;
+    
+    };
+
+void Mahasiswa::cetak(){
+    cout << "Nim = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main()
+{
+    
+}
+
+
