0b979cb452b9aac4c279bf4bce662ef5edab5643
zackynz <nurasfanz@gmail.com>
2024-05-13 15:01:29 +0700

    menggunakan data string

--- mbrstatik.cpp
+++ mbrstatik.cpp
@@ -2,47 +2,4 @@
 #include <string>
 using namespace std;
 
-class Mahasiswa 
-{
-public:
-    static int nim;
-    int id;
-    string nama;
 
-    void setID();
-
-    void printA11();
-    Mahasiswa(string pnama):nama(pnama)
-    {
-        setID();
-    }
-};
-
-int Mahasiswa::nim = 20;
-
-void Mahasiswa::setID()
-{
-    id = nim++;
-}
-
-void Mahasiswa::printA11()
-{
-    cout << "ID   = " << id << endl;
-    cout << "Nama = " << nama << endl;
-    cout << endl;
-}
-
-int main()
-{
-    Mahasiswa mhs1("Lia Kurinia");
-    Mahasiswa mhs2("Asroni");
-    Mahasiswa mhs3("Andi Kurniawan");
-    Mahasiswa mhs4("Joko Purbo");
-
-    mhs1.printA11();
-
-    mhs2.printA11();
-    mhs3.printA11();
-    mhs4.printA11();
-    return 0;
-}
