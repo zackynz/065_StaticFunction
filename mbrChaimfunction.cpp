3ac46dad08f897e1d8cda60b79fe51c3b6624287
zackynz <nurasfanz@gmail.com>
2024-05-13 19:10:35 +0700

    setJudul(string judul): Metode ini menerima parameter string judul. Mengatur atribut judul objek dengan nilai judul yang diberikan. Mengembalikan referensi ke objek itu sendiri (*this) untuk

--- mbrChaimfunction.cpp
+++ mbrChaimfunction.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
-#include <string>
 using namespace std;
+class buku
+{
+    string judul;
+
+public:
+    buku setJudul(string judul)
+    {
+        this->judul = judul;
+        return *this; // vhain function
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
+} bukunya;
+
