597792dedb7e9c537aa6c3884e3b9606ceb5bd9c
zackynz <nurasfanz@gmail.com>
2024-05-13 19:04:10 +0700

    finish

--- mbrconstruktor.cpp
+++ mbrconstruktor.cpp
@@ -8,42 +8,55 @@ private:
     int nim;
     string nama;
 
-private:
-    Mahasiswa()
-    {
-        nim = 0;
-        nama = "";
-    };
-    Mahasiswa(int iNim)
-    {
-        nim = iNim;
-    
-    };
+public:
+    Mahasiswa();
+    Mahasiswa(int);
     Mahasiswa(string);
     Mahasiswa(int iNim, string iNama);
     void cetak();
 };
 
-Mahasiswa::Mahasiswa(string iNama){
-    nama = iNama;
+Mahasiswa::Mahasiswa(){
+    nim = 0;
+    nama = "";
 }
+
+Mahasiswa::Mahasiswa(int iNim)
+    {
+        nim = iNim;
+    };
+
+Mahasiswa::Mahasiswa(int iNama)
+    {
+        nama = iNama;
+    };
 
 Mahasiswa::Mahasiswa(int iNim, string iNama)
     {
         nim = iNim;
         nama = iNama;
-    
     };
-
 void Mahasiswa::cetak(){
-    cout << "Nim = " << nim << endl;
+    cout << endl << "Nim = " << nim << endl;
     cout << "Nama = " << nama << endl;
-    cout << endl;
 }
+
 
 int main()
 {
-    
+    Mahasiswa mhs1;
+    Mahasiswa mhs2(20);
+    Mahasiswa mhs3("Ronaldo");
+    Mahasiswa mhs4(30, "Messi");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
 }
+
+
 
 
